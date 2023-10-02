ab2ede8b1e8c8d0d6c586bf576a3b03e6a63cc5c
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-10-02 22:28:45 +0800

    3 to 9

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const oneButton=document.getElementById('oneButton');
 const twoButton=document.getElementById('twoButton');
+const threeButton=document.getElementById('threeButton');
+const fourButton=document.getElementById('fourButton');
+const fiveButton=document.getElementById('fiveButton');
+const sixButton=document.getElementById('sixButton');
+const sevenButton=document.getElementById('sevenButton');
+const eightButton=document.getElementById('eightButton');
+const nineButton=document.getElementById('nineButton');
+const zeroButton=document.getElementById('zeroButton');
 const plusButton=document.getElementById('plusButton');
+const minusButton=document.getElementById('minusButton');
 const equalButton=document.getElementById('equalButton');
 var calculatorDisplay=document.getElementById('calculatorDisplay');
 var char="";
@@ -15,8 +24,63 @@ twoButton.onclick=function(){
     calculatorDisplay.innerHTML=char;
 }
 
+threeButton.onclick=function(){
+    char=char+parseInt(threeButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
+fourButton.onclick=function(){
+    char=char+parseInt(fourButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
+fiveButton.onclick=function(){
+    char=char+parseInt(fiveButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
+sixButton.onclick=function(){
+    char=char+parseInt(sixButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
+oneButton.onclick=function(){
+    char=char+parseInt(oneButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
+sevenButton.onclick=function(){
+    char=char+parseInt(sevenButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
+eightButton.onclick=function(){
+    char=char+parseInt(eightButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
+nineButton.onclick=function(){
+    char=char+parseInt(nineButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
+oneButton.onclick=function(){
+    char=char+parseInt(oneButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
+zeroButton.onclick=function(){
+    char=char+parseInt(zeroButton.value);
+    calculatorDisplay.innerHTML=char;
+}
+
 plusButton.onclick=function(){
     char=char+plusButton.value;
+    calculatorDisplay.innerHTML=char;
+}
+
+minusButton.onclick=function(){
+    char=char+minusButton.value;
     calculatorDisplay.innerHTML=char;
 }
 
